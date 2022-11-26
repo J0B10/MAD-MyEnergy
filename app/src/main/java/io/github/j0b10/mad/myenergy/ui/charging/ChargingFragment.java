155298826fdd40abe351b15ed13ba03e6a4778eb
Jonas <17405009+J0B10@users.noreply.github.com>
2022-11-26 14:13:07 +0100

    Add Floating Action Buttons to control ev charging.

--- app/src/main/java/io/github/j0b10/mad/myenergy/ui/charging/ChargingFragment.java
+++ app/src/main/java/io/github/j0b10/mad/myenergy/ui/charging/ChargingFragment.java
@@ -1,5 +1,7 @@
 package io.github.j0b10.mad.myenergy.ui.charging;
 
+import static com.google.android.material.color.MaterialColors.getColor;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +12,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.R.attr;
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+
+import io.github.j0b10.mad.myenergy.R;
 import io.github.j0b10.mad.myenergy.databinding.FragmentChargingBinding;
 import io.github.j0b10.mad.myenergy.ui.charging.plan.ChargePlanActivity;
 
@@ -23,8 +29,31 @@ public class ChargingFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         viewModel = new ViewModelProvider(this).get(ChargingViewModel.class);
         binding = FragmentChargingBinding.inflate(inflater, container, false);
-        binding.chargingBtn.setOnClickListener(this::onStartCharging);
+
+        createSpeedDial();
+
+        binding.chargemodeView.setUserInputEnabled(false);
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (binding.chargeBattery.animateSecondaryCharge().isStarted()) {
+            binding.chargeBattery.animateSecondaryCharge().resume();
+        } else {
+            binding.chargeBattery.animateSecondaryCharge().start();
+        }
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        binding.chargeBattery.animateSecondaryCharge().pause();
     }
 
     @Override
@@ -33,8 +62,39 @@ public class ChargingFragment extends Fragment {
         binding = null;
     }
 
-    private void onStartCharging(View view) {
-        startActivity(new Intent(requireContext(), ChargePlanActivity.class));
+    private void createSpeedDial() {
+        binding.speedDial.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.sd_plan_charging, R.drawable.sd_plan)
+                        .setLabel(R.string.sd_lbl_plan)
+                        .setFabBackgroundColor(getColor(binding.speedDial, attr.colorPrimaryContainer))
+                        .setFabImageTintColor(getColor(binding.speedDial, attr.colorOnPrimaryContainer))
+                        .create()
+                , 0);
+        binding.speedDial.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.sd_excess_charging, R.drawable.sd_solar)
+                        .setLabel(R.string.sd_lbl_excess)
+                        .setFabBackgroundColor(getColor(binding.speedDial, attr.colorTertiaryContainer))
+                        .setFabImageTintColor(getColor(binding.speedDial, attr.colorOnTertiaryContainer))
+                        .create()
+                , 1);
+        binding.speedDial.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.sd_stop_charging, R.drawable.sd_stop)
+                        .setLabel(R.string.sd_lbl_stop)
+                        .setFabBackgroundColor(getColor(binding.speedDial, attr.colorErrorContainer))
+                        .setFabImageTintColor(getColor(binding.speedDial, attr.colorOnErrorContainer))
+                        .create()
+                , 2);
+        binding.overlay.setOnClickListener(v -> binding.speedDial.close(true));
+        binding.speedDial.setOnActionSelectedListener(this::onActionSelected);
+    }
+
+    private boolean onActionSelected(SpeedDialActionItem selected) {
+        if (selected.getId() == R.id.sd_plan_charging) {
+            startActivity(new Intent(requireContext(), ChargePlanActivity.class));
+        } else {
+            binding.speedDial.close();
+        }
+        return false;
     }
 
 }
